bc96e04bb2e0ad8a38877a7c105a58f3c7785279
Ayub Aswad <ayubasw@gmail.com>
2020-10-29 21:26:23 +0800

    ref: ♻️ auto parse req body

--- src/config/express.js
+++ src/config/express.js
@@ -3,8 +3,12 @@ const routes = require("./../routes");
 
 const app = express();
 
+app.use(express.json({}));
+
 app.get("/", (req, res) => res.send("Welcome to Library app API."));
 
 app.use("/api", routes);
+
+app.use("*", (req, res) => res.sendStatus(404));
 
 module.exports = app;
